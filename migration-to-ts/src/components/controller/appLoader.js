04a795db2bdeedccd8b7124f2cd066ef7d613bff
Ekaterina Kiryanova <ekaterina.krashennikova@gmail.com>
2023-06-07 16:07:08 +0500

    feat: add apiKey

--- migration-to-ts/src/components/controller/appLoader.js
+++ migration-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'cbf49a28782e49479384dbfa58b8a11b', // получите свой ключ https://newsapi.org/
         });
     }
 }
